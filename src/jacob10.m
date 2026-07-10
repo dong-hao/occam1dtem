7ebc7fbefdcb485ef4766ebf9e57a5682217a08e
dong-hao <donghao@cugb.edu.cn>
2026-07-10 17:32:30 +0800

    optimization/add analytical implicit differentiation
    This replaces the per-layer finite perturbation approach (NL forward calls)
    with an analytical Jacobian derived via implicit differentiation of the
    ABFM fixed-point equation.
    Should (hopefully) be more efficient (one forward call) and reduces the
    truncation error from finite differencing.
    
    src/jacob10.m: replace finite-difference Jacobian with analytical implicit
    differentiation

--- src/jacob10.m
+++ src/jacob10.m
@@ -1,27 +1,100 @@
 function [J, sigma_a] = jacob10(sigma, z, time, A, I)
-% calculate the Jacobian dsigma_a/dsigma for the 1D TEM case
-% with the hard way (with an arbitary pertubation)
-% for dsigma_a/dsigma
+% Analytical Jacobian for 1D TEM central loop (ABFM forward model)
 % used for occam 1D TEM inversion
+% Uses implicit differentiation of the ABFM fixed point
+% instead of the finite difference approximation in the previous 
+% version.
+% 
+% specifically, we have:
+% J(i,j) = d log10(sigma_a_i) / d log10(sigma_j)
+%          (i = time channel, j = layer)
+%
 % DONG Hao
 % 2010/1/7
 % Yuxian, Hebei
-% fixed permeability, for now
-% note that the Jacobian is only related to conductivity (i.e. useful for 
-% smooth inverions only)
+%
 NL = length(sigma);
 NT = length(time);
-% a small value for pertubation
-pert = 1e-6;
 J = zeros(NT, NL);
-sigma_a0 = tem1dfwd10(sigma, z, time, A, I);
-for i = 1:NL % m forward modellings 
-    psigma = sigma;
-    psigma(i) = (1 + pert) * sigma(i);
-    sigma_ap = tem1dfwd10(psigma, z, time, A, I);
-    psigma_a = sigma_ap - sigma_a0;
-    % dsigma_a/dsigma
-    J(:, i) = psigma_a / (pert * sigma(i));
+
+% some duplicated code from tem1dfwd10.m
+mu0 = 4 * pi * 1e-7;
+alpha = 0.6;
+c_const = 1.2;
+z_pad = [z; Inf];
+
+sigma_lin = 10.^sigma;
+
+% ABFM forward with derivative - can be simplified as we already have the 
+% forward calculation in tem1dfwd10.m 
+sigma_a_lin = mean(sigma_lin) * ones(NT, 1);
+relres = 1;
+
+k = 1;
+while (relres > 1e-6 && k < 30)
+    d = sqrt((c_const * time) ./ (sigma_a_lin * mu0));
+    F = zeros(NT, NL);
+    dF_dd = zeros(NT, NL);
+
+    for i = 1:NT % loop over time channels
+        for j = 1:NL % loop over layers
+            z_top = z_pad(j);
+            z_bot = z_pad(j+1);
+            % F1/dF1 at the top interface
+            if z_top <= d(i)
+                uz = z_top / d(i);
+                F1 = (2 - uz) * uz;
+                dF1_dd = -2*z_top/d(i)^2 + 2*z_top^2/d(i)^3;
+            else
+                F1 = 1;
+                dF1_dd = 0;
+            end
+            % F2/dF2 at the bottom interface
+            if isinf(z_bot)
+                F2 = 1;
+                dF2_dd = 0;
+            elseif z_bot <= d(i)
+                uz = z_bot / d(i);
+                F2 = (2 - uz) * uz;
+                dF2_dd = -2*z_bot/d(i)^2 + 2*z_bot^2/d(i)^3;
+            else
+                F2 = 1;
+                dF2_dd = 0;
+            end
+
+            F(i, j) = F2 - F1;
+            dF_dd(i, j) = dF2_dd - dF1_dd;
+        end
+    end
+
+    app_sigma1 = sigma_lin .* F';
+    av_sigma_old = sigma_a_lin;
+    sigma_a_lin = sum(app_sigma1, 1)';
+    sigma_a_lin = alpha * sigma_a_lin + (1 - alpha) * av_sigma_old;
+    relres = norm(av_sigma_old - sigma_a_lin) / norm(av_sigma_old);
+    k = k + 1;
 end
-sigma_a = sigma_a0;
-return
+
+% ---------- Analytical Jacobian via implicit differentiation ---------- %
+% At the fixed point we have sigma_a = sum_j ( sigma_j * F_j(sigma_a) )
+% Implicit differentiation gives:
+%   d sigma_a / d sigma_j = F_j / ( 1 - sum_k sigma_k * dF_k/d(sigma_a) )
+%
+% Chain rule: 
+%   dF_k/d(sigma_a) = dF_k/dd * dd/d(sigma_a)
+%   d = sqrt(c*t / (mu0 * sigma_a))  -->  dd/d(sigma_a) = -d / (2 * sigma_a)
+%
+% So we have:  
+%       C = 1 - sum_k ( sigma_k * dF_k/dd * (-d/(2*sigma_a)) )
+%         = 1 + (d/(2*sigma_a)) * sum_k ( sigma_k * dF_k/dd )
+
+for i = 1:NT
+    d_i = d(i);
+    s_a_i = sigma_a_lin(i);
+    C_i = 1 + (d_i / (2 * s_a_i)) * sum(sigma_lin .* dF_dd(i, :)');
+    % J(i,j) = (sigma_j / sigma_a_i) * (F_ij / C_i)
+    J(i, :) = (sigma_lin' / s_a_i) .* (F(i, :) / C_i);
+end
+
+sigma_a = log10(sigma_a_lin);
+end
